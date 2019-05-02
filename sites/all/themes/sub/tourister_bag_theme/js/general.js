2af81f7831f765e718a7eb76d431908a0e3c9db2
mahak <mahak.jain@innoraft.com>
2019-05-02 19:08:27 +0530

    theming, popup

--- sites/all/themes/sub/tourister_bag_theme/js/general.js
+++ sites/all/themes/sub/tourister_bag_theme/js/general.js
@@ -1,76 +1,44 @@
 (function($) {
-  $(document).ready(function(){
-     $("#block-block-1").hide();
+  Drupal.behaviors.regsiter_ngo = {
+    attach: function (context, settings) {
 
-    // $('#block-views-view-banner-block-1 .view-content').slick({
-    //   arrows: false,
-    //   slidesToShow:1,
-    //   slidesToScroll: 1,
-    //   autoplay: true,
-    //   autoplaySpeed: 10000,
-    //   dots:true,
-    //   pauseOnHover: true,
-    //   pauseOnFocus: false
-    // });
-    //
-    //
-    // $('#block-views-view-banner-block-1 .view-content .views-row').not('.slick-initialized').slick({
-    //   arrows: true,
-    //   slidesToShow:3,
-    //   slidesToScroll: 1,
-    //   dots:false,
-    //   infinite: false,
-    //   responsive: [
-    //     {
-    //       breakpoint: 992,
-    //       settings: {
-    //         slidesToShow: 2,
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 500,
-    //       settings: {
-    //         slidesToShow: 1,
-    //       }
-    //     }
-    //   ]
-    // });
-//     var tax = 0;
-//     var tax_type = '';
-//     var total_amount = $(".page-user-orders.page-user-orders- .component-type-commerce-price-formatted-amount  td.component-total").text();
-//     $(".page-user-orders.page-user-orders- tr.all-taxes").each(function( index ) {
-         
-//          tax_type += $( this ).text() + "+";
-//          total_tax = $("tr.all-taxes td.component-total:first").text();
-//          total_tax = total_tax.replace(',','');
-//          console.log(total_tax);
+      // Show Popup or not
+      if (typeof Drupal.settings.accessories_controller !== 'undefined' && typeof Drupal.settings.accessories_controller.show_popup !== 'undefined') {
+        var show_popup = Drupal.settings.accessories_controller.show_popup;
+        console.log(show_popup);
+        if (show_popup == 1) {
+          $('.region-content .popup-custom').show();
+        }
+        else {
+          $('.region-content .popup-custom').hide();
+        }
+      }
 
-//          tax += parseFloat(total_tax.match(/[\d\.]+/));
-//          console.log(tax);
+      // Popup js
+      if ($('.region-content .popup-custom').length) {
+        // close button
+        $('.region-content .popup-custom .close').click(function() {
+          $('.popup-custom').fadeOut();
+        });
 
-// });
-// tax_type = tax_type.slice(0,-1);
-// var amount = $(".page-user-orders.page-user-orders- .view-commerce-line-item-table td.views-field-commerce-total").text();
-// var taxes = '<div class="bill-summary container"><div class="total-amount-with-GST"> Total Amount: ' + amount +  '+ GST:  ₹' + tax + ' = '+ total_amount +'</div><div class="total-taxes-type">' + tax_type + '= ₹' +  tax + '</div></div>';
-
-// $(".page-user-orders.page-user-orders- .bank-details").before(taxes);
-// $(".page-user-orders.page-user-orders- .field-name-commerce-customer-billing").insertBefore(".page-user-orders.page-user-orders- .view-header .order-date-time.invoice-number");
-
-// $(".page-user-orders.page-user-orders- .view-header .gstinno").insertAfter(".page-user-orders.page-user-orders- .field-name-commerce-customer-billing .field-name-field-phone-no");
-
-// $(".page-user-orders.page-user-orders- .view-header .narration-section").insertAfter(".page-user-orders.page-user-orders- .bill-summary");
-
-
-
-
-// $("body .print-button .print").click(function() {
-//    window.print(); 
-// });
-
-// $(".page-node-add-advance-booking .field-name-field-select-product .form-select option[value='commerce_discount']").remove();
-// $(".page-node-add-advance-booking .field-name-field-select-product .form-select option[value='shipping']").remove();
-// $(".page-node-add-advance-booking .field-name-field-select-product .form-select option[value='product_discount']").remove();
-
-
-  });
+        // Accept terms - Send ajax request
+        if ($('.region-content .popup-custom .accept a').length) {
+          $('.region-content .popup-custom .accept a').on('click', function(e) {
+            $.ajax({
+              url: "/accept-terms-conditions",
+              cache: false,
+              success: function(data){
+                if (data == 'user-accepted') {
+                  $('.popup-custom .popup-custom-inner').html('Thank you for accepting out terms and conditons. Now you can order from our website.');
+                  setTimeout(function(){
+                    $('.popup-custom').fadeOut();
+                  }, 1000);
+                }
+              }
+            });
+          });
+        }
+      }
+    }
+  }
 })(jQuery);
